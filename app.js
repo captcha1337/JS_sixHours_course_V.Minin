ab948fa4705347c38ea21170401ef228ff4793ff
captcha1337 <nissans2000@gmail.com>
2022-05-05 23:00:31 +0300

    ...

---
+++ app.js
@@ -0,0 +1,80 @@
+// 1 variables
+
+// const name = `Ihor`; // shouldn't be used at all
+// const lastName = 'Kurbanov'; // typeof string
+// const age = 22; //typeof number
+// const isProgrammer = true; // typeof boolean
+//
+// // name = 'lolkekek';
+//
+// console.log(name);
+//
+// const _ = true
+//
+// // var names starts only with letter
+//
+// ///////////////////////////// 2 Mutation
+//
+// // console.log('persons name ' + name + ', and his age ' + age)
+// console.log('persons name ' +  name + ', and his age ' + age)
+// console.log(age.toString())
+//
+//
+// ////////////////////////////////////////// 3 operators
+//
+//
+// let a = 10
+// let c = 5
+//
+// c += a //// <- = -> c = c + a
+
+//////////////////////////////////////////// 4 data types
+
+// const isProgrammer = true
+// const name = 'Ihor'
+// const age = 22
+// let x
+//
+// console.log(typeof x)
+// console.log(typeof isProgrammer)
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof null) // bug of typeof
+
+////////////////////////////////////////// 5 operators priority
+
+// const fullAge = 22
+// const birthYear = 2000
+// const currentYear = 2022
+//
+// const isFullAge = (currentYear - birthYear) >= fullAge
+//
+// console.log(isFullAge)
+
+/////////////////////////////////////// 6 conditional operators
+
+// const courseStatus = 'fail'
+//
+// if (courseStatus === 'ready') {
+//     console.log('курс уже готов и его можна проходить')
+// } else if (courseStatus === 'pending') {
+//     console.log('курс пока находится в процессе разработки')
+// } else {
+//     console.log('курс не получился')
+// }
+//
+const isReady = false
+
+
+// if (isReady === true) {
+//     console.log('everything is fine')
+// } else {
+//     console.log('everything is not fine')
+// }
+ isReady ? console.log('everything is fine') : console.log('everything is not fine')
+
+
+// // const num1 = 42
+// // const num2 = '42'
+// //
+// // console.log( num1 === num2)
